a81af418b47f6a2426e841574c4f9786c72c29b2
wiede <detailsohk@163.com>
2025-08-12 21:04:00 +0800

    宿題

---
+++ 08-12/大作业/MDK-ARM/222/retarget_io.d
@@ -0,0 +1,4 @@
+222/retarget_io.o: \
+  C:\Users\akamem\AppData\Local\Arm\Packs\ARM\CMSIS-Compiler\2.1.0\source\armcc\retarget_io.c \
+  RTE\_222\RTE_Components.h \
+  C:\Users\akamem\AppData\Local\Arm\Packs\ARM\CMSIS-Compiler\2.1.0\include\retarget_stdout.h
---
+++ 08-12/大作业/MDK-ARM/222/stdout_evr.d
@@ -0,0 +1,4 @@
+222/stdout_evr.o: \
+  C:\Users\akamem\AppData\Local\Arm\Packs\ARM\CMSIS-Compiler\2.1.0\source\stdout_evr.c \
+  C:\Users\akamem\AppData\Local\Arm\Packs\ARM\CMSIS-Compiler\2.1.0\include\retarget_stdout.h \
+  C:\Users\akamem\AppData\Local\Arm\Packs\ARM\CMSIS-View\1.2.0\EventRecorder\Include\EventRecorder.h
